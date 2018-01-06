17321f28e1255f6b2c102787ca6b1bc8a3868907
Andreas Trawöger <atrawog@hexaplant.com>
2018-01-06 21:35:17 +0100

    Set default coordinates if no GCP exists.

--- tasks.py
+++ tasks.py
@@ -50,67 +50,57 @@ def convert(ctx):
                 print ("Skipping",img)
                 continue
 
-            if not exists(tiff_gcp) and exists(points_in):
-                ctx.run ('cp -v {tiff_in} {tiff_gcp}'.format(tiff_in=escape_path(tiff_in),tiff_gcp=escape_path(tiff_gcp)))
-            else:
+            if not exists(points_in):
                 print("Missing",points_in," for",tiff_in)
+
+            if not exists(tiff_gcp):
+                ctx.run ('cp -v {tiff_in} {tiff_gcp}'.format(tiff_in=escape_path(tiff_in),tiff_gcp=escape_path(tiff_gcp)))
 
             if exists(tiff_gcp):
                 if exists(wld_gcp):
                     ctx.run('rm -v {wld}'.format(wld=wld_gcp))
-                print ("Reading",points_in)
-                with open(points_in) as csvfile:
-                    try:
-                        reader = csv.DictReader(csvfile)
-                        gcp=""
-                        for row in reader:
-                            gcp+="-gcp {pixelX} {pixelY} {mapX} {mapY} ".format(pixelX=row['pixelX'],pixelY=abs(float(row['pixelY'])),mapX=row['mapX'],mapY=row['mapY'])
-                        ctx.run('gdal_edit.py -unsetgt -a_srs EPSG:3857 -a_nodata 255  -mo NODATA_VALUES="255 255 255" {gcp} {tiff}'.format(gcp=gcp,tiff=tiff_gcp))
-                    #ctx.run('gdal_edit.py -unsetgt -unsetmd -a_srs EPSG:3857 -a_nodata 255  -mo NODATA_VALUES="255 255 255" {gcp} {tiff}'.format(gcp=gcp,tiff=tiff_gcp))
-                    #ctx.run('gdal_edit.py -unsetgt -unsetmd -a_srs EPSG:3857 -mo NODATA_VALUES="255 255 255" {gcp} {tiff}'.format(gcp=gcp,tiff=tiff_gcp))
-                    except:
-                        print("Failed to read point file")
-                        continue
 
-                    try:
-                        dataset = gdal.Open(tiff_gcp)
-                        gcps = dataset.GetGCPs()
-                        geotransform = gdal.GCPsToGeoTransform(gcps)
+                if exists(points_in):
+                    print ("Reading",points_in)
+                    with open(points_in) as csvfile:
+                        try:
+                            reader = csv.DictReader(csvfile)
+                            gcp=""
+                            for row in reader:
+                                gcp+="-gcp {pixelX} {pixelY} {mapX} {mapY} ".format(pixelX=row['pixelX'],pixelY=abs(float(row['pixelY'])),mapX=row['mapX'],mapY=row['mapY'])
+                            ctx.run('gdal_edit.py -unsetgt -a_srs EPSG:3857 -a_nodata 255  -mo NODATA_VALUES="255 255 255" {gcp} {tiff}'.format(gcp=gcp,tiff=tiff_gcp))
+                        except:
+                            print("Failed to read point file")
+                            continue
 
-                        ulx, xres, xskew, uly, yskew, yres  = gdal.GCPsToGeoTransform( gcps)
-                        lrx = ulx + (dataset.RasterXSize * xres)
-                        lry = uly + (dataset.RasterYSize * yres)
+                        try:
+                            dataset = gdal.Open(tiff_gcp)
+                            gcps = dataset.GetGCPs()
+                            geotransform = gdal.GCPsToGeoTransform(gcps)
 
-                        proj3857 = Proj(init='epsg:3857')
-                        proj4326 = Proj(init='epsg:4326')
-                        west,north = transform(proj3857,proj4326,ulx,uly)
-                        east,south = transform(proj3857,proj4326,lrx,lry)
-                    except:
-                        print("Failed to read gcp points")
-                        continue
-                if not exists(tiff_final):
-                    ctx.run('gcps2wld.py {tiff} > {wld_out}'.format(tiff=tiff_gcp,wld_out=wld_gcp))
-                    ctx.run('gdal_translate -a_srs EPSG:3857 -mo NODATA_VALUES="255 255 255" {tiff_in} {tiff_out}'.format(tiff_in=tiff_gcp,tiff_out=tiff_wld))
-                    ctx.run("listgeo {tiff_in} > {gtxt_out}".format(tiff_in=escape_path(tiff_wld),gtxt_out=escape_path(gtxt_out)))
+                            ulx, xres, xskew, uly, yskew, yres  = gdal.GCPsToGeoTransform( gcps)
+                            lrx = ulx + (dataset.RasterXSize * xres)
+                            lry = uly + (dataset.RasterYSize * yres)
 
+                            proj3857 = Proj(init='epsg:3857')
+                            proj4326 = Proj(init='epsg:4326')
+                            west,north = transform(proj3857,proj4326,ulx,uly)
+                            east,south = transform(proj3857,proj4326,lrx,lry)
+                        except:
+                            print("Failed to read gcp points")
+                            continue
 
-                    ctx.run("vips --vips-progress  --vips-concurrency=16 im_vips2tiff {tiff_in} {tiff_out}:deflate,tile:256x256,pyramid".format(tiff_in=escape_path(tiff_gcp),tiff_out=escape_path(tiff_final)))
-                    ctx.run("applygeo {gtxt_in} {tiff_out}".format(gtxt_in=escape_path(gtxt_out),tiff_out=escape_path(tiff_final)))
-                    ctx.run('gdal_edit.py -a_nodata 255 -mo NODATA_VALUES="255 255 255" {tiff_out}'.format(tiff_out=tiff_final))
+                else:
+                    ctx.run('gdal_edit.py -a_srs EPSG:3857 -a_nodata 255  -mo NODATA_VALUES="255 255 255" -a_ullr 0 -20000000 20026376 -20026376.39 {tiff}'.format(tiff=tiff_gcp))
 
-                    #if exists(tiff_final):
-                    #    ctx.run('rm -v {tiff}'.format(tiff=tiff_gcp))
-                    #    ctx.run('rm -v {tiff}'.format(tiff=tiff_wld))
-                    #    ctx.run('rm -v {wld}'.format(wld=wld_gcp))
+                ctx.run('gcps2wld.py {tiff} > {wld_out}'.format(tiff=tiff_gcp,wld_out=wld_gcp))
+                ctx.run('gdal_translate -a_srs EPSG:3857 -mo NODATA_VALUES="255 255 255" {tiff_in} {tiff_out}'.format(tiff_in=tiff_gcp,tiff_out=tiff_wld))
+                ctx.run("listgeo {tiff_in} > {gtxt_out}".format(tiff_in=escape_path(tiff_wld),gtxt_out=escape_path(gtxt_out)))
 
-                     #ctx.run('gdal_edit.py -unsetgt -unsetmd -a_srs EPSG:3857 -a_nodata 255  -mo NODATA_VALUES="255 255 255" {gcp} {tiff}'.format(gcp=gcp,tiff=tiff_vips))
-                     #ctx.run('gdal_edit.py -unsetgt -a_srs EPSG:3857 -a_nodata 255  -mo NODATA_VALUES="255 255 255" {gcp} {tiff}'.format(gcp=gcp,tiff=tiff_vips))
-
-
-                #if not exists(tiff_warp):
-                #    ctx.run('gdalwarp  -dstalpha -tps -co "COMPRESS=DEFLATE" -co BIGTIFF=YES -co TILED=YES -r lanczos  -s_srs EPSG:3857 -t_srs EPSG:3857  -multi -wo NUM_THREADS=ALL_CPU {tiff_gcp} {tiff_warp}'.format(tiff_gcp=tiff_gcp,tiff_warp=tiff_warp))
-                    #ctx.run('gdalwarp  -dstalpha  -co "COMPRESS=DEFLATE"  -tps -refine_gcps 10 30 -s_srs EPSG:3857 -t_srs EPSG:3857  -multi -wo NUM_THREADS=ALL_CPU {tiff_gcp} {tiff_warp}'.format(tiff_gcp=tiff_gcp,tiff_warp=tiff_warp))
-                    #ctx.run('gdaladdo  --config COMPRESS_OVERVIEW DEFLATE -r lanczos {tiff_warp} 2 4 8 16 32'.format(tiff_warp=tiff_warp))
+            if not exists(tiff_final):
+                ctx.run("vips --vips-progress  --vips-concurrency=16 im_vips2tiff {tiff_in} {tiff_out}:deflate,tile:256x256,pyramid".format(tiff_in=escape_path(tiff_gcp),tiff_out=escape_path(tiff_final)))
+                ctx.run("applygeo {gtxt_in} {tiff_out}".format(gtxt_in=escape_path(gtxt_out),tiff_out=escape_path(tiff_final)))
+                ctx.run('gdal_edit.py -a_nodata 255 -mo NODATA_VALUES="255 255 255" {tiff_out}'.format(tiff_out=tiff_final))
 
 
 
