ea0141b200bdea7568fe68c2c9e5f706eddf8b36
Andreas Trawöger <atrawog@hexaplant.com>
2018-01-10 22:03:22 +0100

    Add Dublin Core.

---
+++ dublincore.py
@@ -0,0 +1,57 @@
+#_ -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+HEADER="""
+<?xml version="1.0" encoding="UTF-8"?>
+"""
+
+METADATA="""
+<metadata
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns:dc="http://purl.org/dc/elements/1.1/"
+xmlns:dcterms="http://purl.org/dc/terms/">
+
+<dc:title>{a331_a} : {a335_a} </dc:title>
+<dcterms:alternative>{a341_a} : {a343_a} </dcterms:alternative>
+<dcterms:alternative>{a345_a} : {a347_a} </dcterms:alternative>
+<dcterms:alternative>{a370aa}</dcterms:alternative>
+<dc:title>{a359_a}</dc:title>
+<dc:creator>{a100_p}; {a100_d} {a100_4}</dc:creator>
+<dc:creator>{a104ap}; {a104ad} {a104a4}</dc:creator>
+<dc:creator>{a108ap}; {a108ad} {a108a4}</dc:creator>
+<dc:creator>{a112ap}; {a112ad} {a112a4}</dc:creator>
+<dc:creator>{a200_k}; {a200_h} {a200_4}</dc:creator>
+<dc:creator>{a204ak}; {a204ah} {a204a4}</dc:creator>
+<dc:creator>{a208ak}; {a208ah} {a208a4}</dc:creator>
+<dc:subject>{a064aa}W</dc:subject>
+<dc:description>{a407_a}</dc:description>
+<dc:description>{a433_a} + {a437_a}; {a435_a}</dc:description>
+<dc:description>{a439_d}</dc:description>
+<dc:description>{a501_a}W</dc:description>
+<dcterms:abstract>{abstract}</dcterms:abstract>
+<dc:publisher>{a419_b}; {a419_a};{a419_c}</dc:publisher>
+<dc:contributor>{a100bp}; {a100bd} {a100b4}</dc:contributor>
+<dc:contributor>{a104bp}; {a104bd} {a104b4}</dc:contributor>
+<dc:contributor>{a108bp}; {a108bd} {a108b4}</dc:contributor>
+<dc:contributor>{a112bp}; {a112bd} {a112b4}</dc:contributor>
+<dc:contributor>{a200bk}; {a200bh} {a200b4}</dc:contributor>
+<dc:contributor>{a204bk}; {a204bh} {a204b4}</dc:contributor>
+<dc:contributor>{a208bk}; {a208bh} {a208b4}</dc:contributor>
+<dc:contributor>{a677_p}; {a677_d} {a677_4}</dc:contributor>
+<dc:date>{a425a_}</dc:date>
+<dc:type xsi:type="dcterms:DCMIType">Image</dc:type>
+<dc:format xsi:type="dcterms:IMT">TIFF</dc:format>
+<dc:identifier>{a001_a}</dc:identifier>
+<dc:source>http://sammlung.woldan.oeaw.ac.at/</dc:source>
+<dc:language xsi:type="dcterms:ISO639-3">{a037ba}</dc:language>
+<dcterms:isPartOf>{a010_a}</dcterms:isPartOf>
+<dcterms:isPartOf>{a453ma}</dcterms:isPartOf>
+<dcterms:isPartOf>{a453ra}</dcterms:isPartOf>
+<dcterms:isPartOf>{a599_a}</dcterms:isPartOf>
+<dc:coverage xsi:type="dcterms:Box">westlimit={a034_d}; southlimit={a034_g}; eastlimit={a034_e}; northlimit={a034_f}</dc:coverage>
+<dcterms:license xsi:type="dcterms:URI">https://creativecommons.org/licenses/by/4.0/</dcterms:license>
+<dcterms:provenance>http://sammlung.woldan.oeaw.ac.at/</dcterms:provenance>
+<dcterms:rightsholder>OAW Sammlung Woldan</dcterms:rightsholder>
+</metadata>
+"""
